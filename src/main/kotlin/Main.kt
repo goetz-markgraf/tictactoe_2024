60fe25bf458147ce7730ccdadbc2930ed3d84ed7
Arsen <arsenadamen@gmail.com>
2024-02-05 12:10:00 +0100

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,41 @@
-var playerField= mutableListOf('1','2','3','4','5','6','7','8','9')
+val playerField = mutableListOf('1','2','3','4','5','6','7','8','9')
+val choiceX = mutableListOf(' ',' ',' ',' ',' ')
+val choiceO = mutableListOf(' ',' ',' ',' ',' ')
+
 fun main() {
     showField()
+    placeX()
 }
-fun showField(){
+fun showField() {
     println("+---+---+---+")
-    println("| " + playerField[0]+ " | " +playerField[1]+" | "+playerField[2]+" |")
+    println("| " + playerField[0] + " | " + playerField[1] + " | " + playerField[2] + " |")
     println("+---+---+---+")
-    println("| " + playerField[3]+ " | " +playerField[4]+" | "+playerField[5]+" |")
+    println("| " + playerField[3] + " | " + playerField[4] + " | " + playerField[5] + " |")
     println("+---+---+---+")
-    println("| " + playerField[6]+ " | " +playerField[7]+" | "+playerField[8]+" |")
+    println("| " + playerField[6] + " | " + playerField[7] + " | " + playerField[8] + " |")
     println("+---+---+---+")
+}
+
+fun placeX() {
+    val index = readln().toIntOrNull()
+    if (index != null) {
+        playerField[index - 1] = 'x'
+        showField()
+        placeO()
+    } else {
+        println("Enter a number")
+        placeX()
+    }
+}
+
+fun placeO() {
+    val index = readln().toIntOrNull()
+    if (index != null) {
+        playerField[index - 1] = 'o'
+        showField()
+        placeX()
+    } else {
+        println("Enter a number")
+        placeO()
+    }
 }
