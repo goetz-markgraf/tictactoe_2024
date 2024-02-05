9a9423970c951227e67e0cb0e5160cd3f56a4769
Admin <a.kalinin.de@gmail.com>
2024-02-05 11:35:40 +0100

    PROGRAMME

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
+var playerField= mutableListOf('1','2','3','4','5','6','7','8','9')
 fun main() {
-    println("Hello, World!")
+    showField()
+}
+fun showField(){
+    println("+---+---+---+")
+    println("| " + playerField[0]+ " | " +playerField[1]+" | "+playerField[2]+" |")
+    println("+---+---+---+")
+    println("| " + playerField[3]+ " | " +playerField[4]+" | "+playerField[5]+" |")
+    println("+---+---+---+")
+    println("| " + playerField[6]+ " | " +playerField[7]+" | "+playerField[8]+" |")
+    println("+---+---+---+")
 }
