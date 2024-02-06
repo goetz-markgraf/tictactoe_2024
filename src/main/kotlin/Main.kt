0fda4554bb495a4f0d4c6f0258cffac5bf518bef
Admin <a.kalinin.de@gmail.com>
2024-02-06 15:38:12 +0100

    Fix some bugs. Check function is able to check.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,32 +4,42 @@ enum class Player {
     X, O
 }
 
-var scoreX = 0
-var scoreO = 0
-var shouldPrintScores = false
+enum class Finish {
+    WIN_X, WIN_O, TIE, IN_PROGRESS
+}
+
+
 
 fun main() {
+    print("Enter your name, Player X: ")
+    val playerX = readln()
+    print("Enter your name, Player O: ")
+    val playerO = readln()
     var player = Player.X
     showField()
 
     while (true) {
-        place(player)
-        check(player)
-        player = if (player == Player.X) Player.O else Player.X
+        val validMove = place(player, if (player == Player.X) playerX else playerO)
 
-        if (shouldPrintScores) {
-            println("Score X: $scoreX")
-            println("Score O: $scoreO")
-            break
-        }
+        if (validMove == true) {
+            val state = check(player)
 
-        if (playerField.all { it == 'X' || it == 'O' }) {
-            println("It's a tie!")
+            if (state == Finish.WIN_X) {
+                println("$playerX won!")
+                break
+            } else if (state == Finish.WIN_O) {
+                println("$playerO won!")
+                break
+            } else if (state == Finish.TIE) {
+                println("Tie!")
+                break
+            }
+            player = if (player == Player.X) Player.O else Player.X
+        } else if (validMove == null) {
             break
         }
     }
 }
-
 fun showField() {
     println("+---+---+---+")
     println("| " + playerField[0] + " | " + playerField[1] + " | " + playerField[2] + " |")
@@ -40,25 +50,29 @@ fun showField() {
     println("+---+---+---+")
 }
 
-fun place(player: Player) {
-    print(player.name + " player's turn: ")
-    val index = readLine()?.toIntOrNull()
-
+fun place(player: Player, playerName: String):Boolean? {
+    print("$playerName\'s turn: ")
+    val index = readln().toIntOrNull()
     if (index != null && index in 1..9) {
         if (playerField[index - 1] !in listOf('X', 'O')) {
             playerField[index - 1] = if (player == Player.X) 'X' else 'O'
             showField()
+            return true
         } else {
             println("The cell is already taken!")
-            return
+            return false
         }
+    } else if (index.toString() == "stopp") {
+        return null
+    } else if (index.toString() == "neu") {
+        return true
     } else {
         println("Enter a number between 1 and 9")
-        return
+        return false
     }
 }
 
-fun check(player: Player) {
+fun check(player: Player): Finish {
     val symbolToCheck = if (player == Player.X) 'X' else 'O'
 
     if (playerField[0] == symbolToCheck && playerField[1] == symbolToCheck && playerField[2] == symbolToCheck ||
@@ -70,12 +84,12 @@ fun check(player: Player) {
         playerField[0] == symbolToCheck && playerField[4] == symbolToCheck && playerField[8] == symbolToCheck ||
         playerField[2] == symbolToCheck && playerField[4] == symbolToCheck && playerField[6] == symbolToCheck
     ) {
-        if (player == Player.X) {
-            scoreX++
-        } else {
-            scoreO++
-        }
-        shouldPrintScores = true
+        return if (player == Player.X) return Finish.WIN_X else return Finish.WIN_O
+    }
+    else if (playerField.all{it=='X' || it=='O'}) {
+        return Finish.TIE
+    }
+    else {
+        return Finish.IN_PROGRESS
     }
 }
-//Upload By Artemis
